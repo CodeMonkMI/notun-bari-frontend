721ce1bcf568647e8bc10db9b1255676f98bbfb0
Monirul Islam <mr.monirul.dev@gmail.com>
2025-08-25 19:52:05 +0600

    logout button on profile menu added

--- src/features/profile/features/view/components/profile/Menu.tsx
+++ src/features/profile/features/view/components/profile/Menu.tsx
@@ -1,7 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { useLogout } from "@/lib/api/auth";
+import { useAuthContext } from "@/store/authStore";
 import { LogOut, Settings, ShieldCheck, UserCog } from "lucide-react";
+import { useNavigate } from "react-router";
 
 export const Menu = () => {
+  const { clear } = useAuthContext();
+  const logout = useLogout();
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    logout();
+    clear();
+    navigate("/auth/login");
+  };
   return (
     <div>
       <div className="space-y-2">
@@ -14,7 +25,11 @@ export const Menu = () => {
         <Button variant="outline" className="w-full justify-start">
           <ShieldCheck className="mr-2 h-4 w-4" /> Security
         </Button>
-        <Button variant="destructive" className="w-full justify-start">
+        <Button
+          variant="destructive"
+          className="w-full justify-start cursor-pointer"
+          onClick={logoutHandler}
+        >
           <LogOut className="mr-2 h-4 w-4" /> Logout
         </Button>
       </div>
