cb0d3ad13977a0365e06376567e3bdab59e65dd3
Monirul Islam <mr.monirul.dev@gmail.com>
2025-08-26 00:27:53 +0600

    update navbar menu

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,40 +1,47 @@
+import { useAuthContext } from "@/store/authStore";
+import { Link } from "react-router";
+
 const Navbar = () => {
+  const { isAuthenticated, isHydrated } = useAuthContext();
   return (
-    <nav className="bg-white shadow-md w-full z-50">
+    <nav className="bg-black shadow-md w-full z-50">
       <div className="container mx-auto flex items-center justify-between py-4 px-6">
-        <h1 className="text-2xl font-bold">
-          United <span className="text-primary">Pets</span>
-        </h1>
+        <Link className="text-2xl font-bold" to={"/"}>
+          Notun<span className="text-blue-400">Bari</span>
+        </Link>
         <ul className="flex space-x-6 font-medium">
           <li>
-            <a href="#" className="hover:text-primary">
+            <Link to="/" className="hover:text-primary">
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#services" className="hover:text-primary">
-              Services
-            </a>
+            <Link to="/pets" className="hover:text-primary">
+              Pets
+            </Link>
           </li>
+
           <li>
-            <a href="#about" className="hover:text-primary">
+            <Link to="/about" className="hover:text-primary">
               About
-            </a>
+            </Link>
           </li>
+
           <li>
-            <a href="#adopt" className="hover:text-primary">
-              Adopt
-            </a>
-          </li>
-          <li>
-            <a href="#gallery" className="hover:text-primary">
-              Gallery
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="hover:text-primary">
+            <Link to="/contact" className="hover:text-primary">
               Contact
-            </a>
+            </Link>
+          </li>
+          <li>
+            {isHydrated && isAuthenticated ? (
+              <Link to={"/dashboard"} className="hover:text-primary">
+                Dashboard
+              </Link>
+            ) : (
+              <Link to={"/auth/login"} className="hover:text-primary">
+                Login
+              </Link>
+            )}
           </li>
         </ul>
       </div>
